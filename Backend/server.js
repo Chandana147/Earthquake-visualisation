a191286c208ec1769531b7071702dce0984c50d0
Kakara Chaitanya Chandana <chandana@Kakaras-MacBook-Air.local>
2023-07-14 15:43:24 +0530

    added new origin in cors

--- Backend/server.js
+++ Backend/server.js
@@ -12,7 +12,7 @@ const earthquake = require('./earthquake');
 const cors=require('cors');
 
 app.use(cors({
-  origin: ['http://localhost:3000',"https://earthquake-gqqp.onrender.com"]
+  origin: ['http://localhost:3000',"https://earthquake-visualisation.vercel.app"]
 }));
 app.get('/fetchedData', async (req, res) => {
   try {
